728a6d5d228a7cc3f6ba58b5166de6500b4b1cff
Rohan <Rohan@MacBook-Pro.local>
2023-02-04 17:05:25 -0700

    updated pathing

---
+++ target/doc/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"algae":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"algae/mapping/enum.PropertyError.html\" title=\"enum algae::mapping::PropertyError\">PropertyError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ target/doc/implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"algae":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"algae/mapping/enum.PropertyType.html\" title=\"enum algae::mapping::PropertyType\">PropertyType</a>&lt;T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

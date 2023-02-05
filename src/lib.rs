062c91b924c7f8d671b25ece8740fabeddfa5eb4
Rohan <Rohan@MacBook-Pro.local>
2023-02-04 18:04:39 -0700

    updated crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 #![allow(dead_code)]
 
 //! `algae` is a collection of abstract algebraic structures implemented in
-//! Rust. It begins with the [`AlgaeSet`] and builds up to vector spaces and
-//! Lie groups.
+//! Rust. It begins by defining sets and eventually builds up to vector spaces
+//! and Lie groups.
 
 pub mod algaeset;
 pub mod magma;

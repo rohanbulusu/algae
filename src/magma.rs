6fc1834dced57b98dcb38cb02255906faa5b9163
Rohan <Rohan@MacBook-Pro.local>
2023-02-04 18:03:26 -0700

    added Monoid struct

--- src/magma.rs
+++ src/magma.rs
@@ -28,7 +28,7 @@ use crate::mapping::{BinaryOperation, PropertyType};
 /// ```
 pub struct Magma<'a, T> {
     aset: AlgaeSet<T>,
-    binop: &'a mut dyn BinaryOperation<T>,
+    binop: &'a mut dyn BinaryOperation<T>
 }
 
 impl<'a, T> Magma<'a, T> {
@@ -43,6 +43,83 @@ impl<'a, T> Magma<'a, T> {
 }
 
 impl<'a, T: Copy + PartialEq> BinaryOperation<T> for Magma<'a, T> {
+
+	fn operation(&self) -> &dyn Fn(T, T) -> T {
+		self.binop.operation()
+	}
+
+	fn properties(&self) -> Vec<PropertyType<T>> {
+		self.binop.properties()
+	}
+
+	fn input_history(&self) -> &Vec<T> {
+		self.binop.input_history()
+	}
+
+	fn cache(&mut self, input: T) {
+		self.binop.cache(input);
+	}
+
+
+}
+
+/// A set equipped with an associative binary operation with identity.
+///
+/// This is a representation of the monoids of abstract algebra.
+/// Associativity and identity are required of its binary operation; its
+/// construction involves a set (specifically an [`AlgaeSet`] and a binary 
+/// operation (anything implementing [`BinaryOperation`]) with the
+/// aforementioned properties.
+///
+/// # Examples
+/// 
+/// ```
+/// use algae_rs::algaeset::AlgaeSet;
+/// use algae_rs::mapping::{BinaryOperation, MonoidOperation};
+/// use algae_rs::magma::Monoid; 
+/// 
+/// let mut add = MonoidOperation::new(&|a, b| a + b, 0);
+/// let mut monoid = Monoid::new(
+///     AlgaeSet::<i32>::all(), 
+///     &mut add,
+/// 	0
+/// );
+///
+/// let monoid_sum = monoid.with(1, 2);
+/// assert!(monoid_sum.is_ok());
+/// assert!(monoid_sum.unwrap() == 3);
+/// 
+/// let mut bad_add = MonoidOperation::new(&|a, b| a + b, 1);
+/// let mut bad_monoid = Monoid::new(
+/// 	AlgaeSet::<i32>::all(),
+/// 	&mut bad_add,
+/// 	1
+/// );
+/// 
+/// let bad_monoid_sum = bad_monoid.with(1, 2);
+/// assert!(bad_monoid_sum.is_err());
+/// ```
+pub struct Monoid<'a, T> {
+    aset: AlgaeSet<T>,
+    binop: &'a mut dyn BinaryOperation<T>,
+    identity: T
+}
+
+impl<'a, T: Copy + PartialEq> Monoid<'a, T> {
+
+	pub fn new(aset: AlgaeSet<T>, binop: &'a mut dyn BinaryOperation<T>, identity: T) -> Self {
+		assert!(binop.is(PropertyType::Associative));
+		assert!(binop.is(PropertyType::WithIdentity(identity)));
+		Self {
+			aset,
+			binop,
+			identity
+		}
+	}
+
+}
+
+impl<'a, T: Copy + PartialEq> BinaryOperation<T> for Monoid<'a, T> {
 
 	fn operation(&self) -> &dyn Fn(T, T) -> T {
 		self.binop.operation()
